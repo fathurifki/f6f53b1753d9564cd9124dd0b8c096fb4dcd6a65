143d73baa0e2da2b75216feef025f3d28fc4e8be
fathurifki <rifkifathu@gmail.com>
2020-11-13 09:39:54 +0700

    add more image

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,7 +1,21 @@
 import back from './arrow-back.svg'
+import cart from './cart.svg'
+import arrow from './arrow-right.svg'
+import arrowWhite from './arrow-white.svg'
+import pin from './pin.png'
+import google from './google-2015.svg'
+import close from './closeblack.svg'
+import down from './down.svg'
 
 const image = {
-    back
+    back,
+    cart,
+    arrow,
+    arrowWhite,
+    pin,
+    google,
+    close,
+    down
 }
 
 export default image
