d892cd52182abe02e7b5ec92ea7b0231f8733df6
fathurifki <rifkifathu@gmail.com>
2020-11-13 09:41:21 +0700

    modify main

--- src/view/main/index.tsx
+++ src/view/main/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Header from '../header';
 import Body from '../body';
+import image from 'src/assets/image';
+import Modal from '../modal';
 
 
 
@@ -19,12 +21,27 @@ const Main = () => {
     console.log('TODAY', state)
 
     return (
-        <div className="h-screen m-auto max-w-md bg-gray-200">
-            <div className="flex flex-col h-screen">
+        <div className="bg-gray-200">
+            <div className="h-screen m-auto max-w-md bg-white">
                 <div className="flex flex-col h-screen">
-                    <Header />
-                    <div className="w-full overflow-auto h-screen">
-                    <Body />
+                    <div className="flex flex-col h-screen">
+                        <Header />
+                        <div className="w-full overflow-auto h-screen">
+                            <Body />
+                            {/* <Modal/> */}
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="relative flex rounded flex-row bottom-0 mb-4 justify-between ml-2 mr-2 p-4 max-w-md bg-red-500 text-white">
+                                <div className="flex flex-col">
+                                    <span className="font-bold">5 Items | Rp. 125.000</span>
+                                    <span>Termasuk Ongkos Kirim</span>
+                                </div>
+                                <div className="flex flex-row">
+                                    <img src={image.cart} height={30} width={30}></img>
+                                    <img src={image.arrowWhite} height={30} width={30}></img>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
