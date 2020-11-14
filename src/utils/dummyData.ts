d27c18187ea10904b5f20e9e4e6d26ef1d655828
fathurifki <rifkifathu@gmail.com>
2020-11-14 15:59:14 +0700

    fixing dummy data

--- src/utils/dummyData.ts
+++ src/utils/dummyData.ts
@@ -1,35 +1,79 @@
 import moment from 'moment';
 import 'moment/locale/id'
+import image from 'src/assets/image';
 
 export const data = [
     {
         "id": 1,
-        "name": "Sego Goreng",
-        "image": "https://picsum.photos/200/300",
+        "name": "Sweet Pancake",
+        "image": image.pancake,
         "price": 15000,
-        "date": moment().format("dddd. DD MMMM YYYY")
+        "date": moment().add(2, 'days').format("dddd. DD MMMM YYYY")
     },
     {
         "id": 2,
-        "name": "Sego Bandeng",
-        "image": "https://picsum.photos/200/300",
+        "name": "Nachos With Bachamel Sauce ",
+        "image": image.nachos,
         "price": 25000,
-        "date": moment().format("dddd. DD MMMM YYYY")
+        "date": moment().add(2, 'days').format("dddd. DD MMMM YYYY")
     },
     {
         "id": 3,
-        "name": "Sego Tiwul",
-        "image": "https://picsum.photos/200/300",
+        "name": "Spicy Meatball Aldente",
+        "image": image.meatball,
         "price": 35000,
-        "date": moment().format("dddd. DD MMMM YYYY")
+        "date": moment().add(2, 'days').format("dddd. DD MMMM YYYY")
     },
     {
         "id": 4,
-        "name": "Sego Kucing",
-        "image": "https://picsum.photos/200/300",
+        "name": "Avocado Egg Salad With Holandaise Sauce",
+        "image": image.salad,
         "price": 45000,
-        "date": moment().format("dddd. DD MMMM YYYY")
+        "date": moment().add(2, 'days').format("dddd. DD MMMM YYYY")
     },
+    {
+        "id": 5,
+        "name": "Avocado Egg Salad With Holandaise Sauce",
+        "image": image.salad,
+        "price": 45000,
+        "date": moment().add(2, 'days').format("dddd. DD MMMM YYYY")
+    },
+    {
+        "id": 6,
+        "name": "Sweet Pancake",
+        "image": image.pancake,
+        "price": 15000,
+        "date": moment().add(2, 'days').format("dddd. DD MMMM YYYY")
+    },
+    {
+        "id": 7,
+        "name": "Nachos With Bachamel Sauce ",
+        "image": image.nachos,
+        "price": 25000,
+        "date": moment().add(3, 'days').format("dddd. DD MMMM YYYY")
+    },
+    {
+        "id": 8,
+        "name": "Spicy Meatball Aldente",
+        "image": image.meatball,
+        "price": 35000,
+        "date": moment().add(3, 'days').format("dddd. DD MMMM YYYY")
+    },
+    {
+        "id": 9,
+        "name": "Avocado Egg Salad With Holandaise Sauce",
+        "image": image.salad,
+        "price": 45000,
+        "date": moment().add(3, 'days').format("dddd. DD MMMM YYYY")
+    },
+    {
+        "id": 10,
+        "name": "Avocado Egg Salad With Holandaise Sauce",
+        "image": image.salad,
+        "price": 45000,
+        "date": moment().add(3, 'days').format("dddd. DD MMMM YYYY")
+    },
+
 ]
 
 export const address = [
